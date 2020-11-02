e57800814f64029054ea9b4052b91b019ca2135c
Mahmoud Ibrahim <mahmouim@stud.ntnu.no>
2020-11-02 23:46:40 +0100

    Reinforcement Learning

---
+++ Reinforcement Learning/Oppgave1.py
@@ -0,0 +1,125 @@
+import math
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+buckets=(3, 3, 6, 6)
+num_episodes=1000
+min_lr=0.01
+# Epsilon: Describes the lower bound of exploring, i.e doing something at random
+min_epsilon=0.01
+# Gamma: discount factor between immediate or future rewards
+gamma=0.99
+# Number at which we adjust the epsilon/learning_rate-convergence towards min_lr/min_epsilon
+decay=25
+env = gym.make("CartPole-v0")
+# Used to scale discrete states properly
+upper_bounds = [env.observation_space.high[0], 0.5, env.observation_space.high[2], math.radians(50) / 1.]
+lower_bounds = [env.observation_space.low[0], -0.5, env.observation_space.low[2], -math.radians(50) / 1.]
+Q_table = np.zeros(buckets + (env.action_space.n,))
+steps = np.zeros(num_episodes)
+
+def get_epsilon(t):
+    """
+    Balance out randomness as the number of episodes get higher
+    :param t: the episode
+    :return:
+    """
+    return max(min_epsilon, min(1., 1. - math.log10((t + 1) / decay)))
+
+def get_learning_rate(t):
+    """
+    Balance out the learning rate as the number of episodes get higher
+    :param t: the episode
+    :return:
+    """
+    return max(min_lr, min(1., 1. - math.log10((t + 1) / decay)))
+
+def discretize_state(obs):
+    """
+    Discretize the contionous possible states of the cartpole to approximate by q-value.
+    We do this by adding the otherwise tiny continous values as a range described by the bucket
+    :param obs:
+    :return:
+    """
+    discretized = list()
+    for i in range(len(obs)):
+        scaling = ((obs[i] + abs(lower_bounds[i]))/(upper_bounds[i] - lower_bounds[i]))
+        new_obs = int(round((buckets[i] - 1) * scaling))
+        new_obs = min(buckets[i] - 1, max(0, new_obs))
+        discretized.append(new_obs)
+    return tuple(discretized)
+
+def choose_action(state, e):
+    """Select action from Q if epsilon is high enough"""
+    if (np.random.random() < get_epsilon(e)):
+        return env.action_space.sample()
+    else:
+        return np.argmax(Q_table[state])
+
+def update_q(state, action, reward, new_state, e):
+    """
+    Update q-table by relevant formula.
+    :param state:
+    :param action:
+    :param reward:
+    :param new_state:
+    :param e:
+    :return:
+    """
+    Q_table[state][action] += (get_learning_rate(e) *
+                                    (reward
+                                     + gamma * np.max(Q_table[new_state])
+                                     - Q_table[state][action]))
+def train():
+    for e in range(num_episodes):
+        # Initializes the state
+        current_state = discretize_state(env.reset())
+        done = False
+        # Looping for each step
+        while not done:
+            steps[e] += 1
+            # Choose A from S
+            action = choose_action(current_state, e)
+            # Take action
+            obs, reward, done, _ = env.step(action)
+            new_state = discretize_state(obs)
+            # Update Q(S,A)
+            update_q(current_state, action, reward, new_state, e)
+            current_state = new_state
+
+def plot_learning():
+    sns.lineplot(range(len(steps)),steps)
+    plt.xlabel("Episode")
+    plt.ylabel("Steps")
+    plt.show()
+    t = 0
+    for i in range(num_episodes):
+        if steps[i] > 195:
+            t+=1
+    print(t, "episodes were successfully completed.")
+
+
+
+if __name__ == "__main__":
+
+
+    train()
+    plot_learning()
+    t = 0
+    done = False
+    current_state = discretize_state(env.reset())
+    for i in range(100):
+        current_score = 0
+        env.reset()
+        done = False
+        while not done:
+            # env.render()
+            t = t + 1
+            current_score = current_score + 1
+            action = choose_action(current_state, 500)
+            obs, reward, done, _ = env.step(action)
+            new_state = discretize_state(obs)
+            current_state = new_state
+        print(i, ":", current_score)
+    print("AVG SCORE: {}".format(t/100))
---
+++ Reinforcement Learning/Oppgave2.py
@@ -0,0 +1,147 @@
+import gym
+import random
+import math
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import seaborn as sns
+import matplotlib.pyplot as plt
+from torch import FloatTensor, LongTensor
+# hyper parameterss
+episodes = 500  # number of episodes
+# EPS_START = 0.9  # e-greedy threshold start value
+min_epsilon = 0.01  # e-greedy threshold end value
+decay = int(225 / 8)  # e-greedy threshold decay
+gamma = 0.7  # Q-learning discount factor
+lr = 0.0001  # NN optimizer learning rate
+hidden = 256  # NN hidden layer size
+batch = 64  # Q-learning batch size
+epsilons = []
+random_amounts = 0
+total = 0
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    def push(self, transition):
+        self.memory.append(transition)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    def sample(self, batch):
+        return random.sample(self.memory, batch)
+    def __len__(self):
+        return len(self.memory)
+
+class Network(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.l1 = nn.Linear(4, hidden)
+        self.l2 = nn.Linear(hidden, 2)
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = self.l2(x)
+        return x
+
+env = gym.make('CartPole-v0')
+model = Network()
+memory = ReplayMemory(10000)
+optimizer = optim.Adam(model.parameters(), lr)
+episode_durations = []
+def get_epsilon(t):
+    """
+    Balance out randomness as the number of episodes get higher
+    :param t: the episode
+    :return:
+    """
+    return max(min_epsilon, min(1., 1. - math.log10((t + 1) / decay)))
+
+def select_action(state, e):
+    global total
+    global random_amounts
+    # sample = random.random()
+    # eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY
+    # return model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+    total = total + 1
+    if np.random.random() < get_epsilon(e):
+        random_amounts = random_amounts + 1
+        return LongTensor([[env.action_space.sample()]])
+    else:
+        # print(model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1])
+        return model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+
+def run_episode(e, environment, demo=False):
+    state = environment.reset()
+    steps = 0
+    while True:
+        # action = select_action(FloatTensor([state]), e)
+        action = select_action(FloatTensor([state]), e)
+        next_state, reward, done, _ = environment.step(action[0, 0].numpy())
+        # negative reward when attempt ends
+        if done:
+            reward = - 1
+            """if steps < 195:
+                reward = -1
+            else:
+                reward += 1"""
+        memory.push((FloatTensor([state]),
+                     torch.tensor(action),  # action is already a tensor
+                     FloatTensor([next_state]),
+                     FloatTensor([reward])))
+        if not demo:
+            learn()
+        state = next_state
+        steps += 1
+        if done:
+            print(e, steps)
+            epsilons.append(get_epsilon(e)*200)
+            episode_durations.append(steps)
+            break
+
+def learn():
+    if len(memory) < batch:
+        return
+    # random transition batch is taken from experience replay memory
+    transitions = memory.sample(batch)
+    batch_state, batch_action, batch_next_state, batch_reward = zip(*transitions)
+    batch_state = Variable(torch.cat(batch_state))
+    batch_action = Variable(torch.cat(batch_action))
+    batch_reward = Variable(torch.cat(batch_reward))
+    batch_next_state = Variable(torch.cat(batch_next_state))
+    # current Q values are estimated by NN for all actions
+    current_q_values = model(batch_state).gather(1, batch_action)
+    # expected Q values are estimated from actions which gives maximum Q value
+    max_next_q_values = model(batch_next_state).detach().max(1)[0]
+    expected_q_values = batch_reward + (gamma * max_next_q_values)
+    loss = F.smooth_l1_loss(current_q_values, expected_q_values.reshape(64, 1))
+    # backpropagation of loss to NN
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+def plot_durations(label_string):
+        sns.lineplot(range(len(episode_durations)), episode_durations)
+        sns.lineplot(range(len(epsilons)), epsilons)
+        plt.xlabel("Episodes - %s" % label_string)
+        plt.ylabel("Steps")
+        plt.show()
+        t = 0
+        for i in range(episodes):
+            if episode_durations[i] > 195:
+                t += 1
+        print(t, "episodes were successfully completed.")
+
+for e in range(episodes):
+    run_episode(e, env)
+plot_durations("Training")
+old_episodes = episodes
+episodes = 100
+episode_durations = []
+epsilons = []
+for e in range(100):
+    run_episode(old_episodes, env, demo=True)
+plot_durations("Testing")
+print('Average Score {}'.format(round(sum(episode_durations)/100)))
+plt.show()
