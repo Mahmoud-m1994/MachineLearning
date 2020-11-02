3205090bd3419653bd015e92239ea7cb70180c97
Mahmoud Ibrahim <mahmouim@stud.ntnu.no>
2020-11-02 23:36:31 +0100

    convolutional neural networks

---
+++ convolutional neural networks/Notes.py
@@ -0,0 +1,72 @@
+# import standard PyTorch modules
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter # TensorBoard support
+
+# import torchvision module to handle image manipulation
+import torchvision
+import torchvision.transforms as transforms
+
+# calculate train time, writing train data to files etc.
+import time
+import pandas as pd
+import json
+from IPython.display import clear_output
+
+torch.set_printoptions(linewidth=120)
+torch.set_grad_enabled(True)     # On by default, leave it here for clarity
+
+
+
+# Use standard FashionMNIST dataset
+train_set = torchvision.datasets.FashionMNIST(
+    root = './data/FashionMNIST',
+    train = True,
+    download = True,
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+
+
+# Build the neural network, expand on top of nn.Module
+class Network(nn.Module):
+  def __init__(self):
+    super().__init__()
+
+    # define layers
+    self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+    self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+
+    self.fc1 = nn.Linear(in_features=12*4*4, out_features=120)
+    self.fc2 = nn.Linear(in_features=120, out_features=60)
+    self.out = nn.Linear(in_features=60, out_features=10)
+
+  # define forward function
+  def forward(self, t):
+    # conv 1
+    t = self.conv1(t)
+    t = F.relu(t)
+    t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+    # conv 2
+    t = self.conv2(t)
+    t = F.relu(t)
+    t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+    # fc1
+    t = t.reshape(-1, 12*4*4)
+    t = self.fc1(t)
+    t = F.relu(t)
+
+    # fc2
+    t = self.fc2(t)
+    t = F.relu(t)
+
+    # output
+    t = self.out(t)
+    # don't need softmax here since we'll use cross-entropy as activation.
+
+    return t
